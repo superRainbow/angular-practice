76c6298036144fde7ffb92d4afe3b75df9b6d3a8
rainbow <g901612002@hotmail.com>
2022-05-08 20:42:34 +0800

    feat: remove api service

--- src/app/chart/bar-chart/bar-chart.component.ts
+++ src/app/chart/bar-chart/bar-chart.component.ts
@@ -3,25 +3,17 @@ import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
 import DataLabelsPlugin from 'chartjs-plugin-datalabels';
-import { ApiService } from 'src/app/api.service';
 
 @Component({
   templateUrl: './bar-chart.component.html',
-  styleUrls: [ './bar-chart.component.scss' ],
+  styleUrls: ['./bar-chart.component.scss'],
 })
 export class BarChartComponent implements OnInit {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
-  constructor(public api: ApiService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.api.getData().subscribe((data:any) => {
-      console.log('init', data);
-      this.sort = data?.sort;
-    });
-  }
-
-  sort = '';
+  ngOnInit() {}
 
   public barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
@@ -29,8 +21,8 @@ export class BarChartComponent implements OnInit {
     scales: {
       x: {},
       y: {
-        min: 10
-      }
+        min: 10,
+      },
     },
     plugins: {
       legend: {
@@ -38,29 +30,39 @@ export class BarChartComponent implements OnInit {
       },
       datalabels: {
         anchor: 'end',
-        align: 'end'
-      }
-    }
+        align: 'end',
+      },
+    },
   };
   public barChartType: ChartType = 'bar';
-  public barChartPlugins = [
-    DataLabelsPlugin
-  ];
+  public barChartPlugins = [DataLabelsPlugin];
 
   public barChartData: ChartData<'bar'> = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
-    ]
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    ],
   };
 
   // events
-  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
     console.log(event, active);
   }
 
-  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
     console.log(event, active);
   }
 
@@ -73,9 +75,9 @@ export class BarChartComponent implements OnInit {
       Math.round(Math.random() * 100),
       56,
       Math.round(Math.random() * 100),
-      40 ];
+      40,
+    ];
 
     this.chart?.update();
   }
 }
-
